47753da874528510a179803dd7120b9267d1d006
IntsiraL <martin.rasoloniaina@yahoo.fr>
2017-05-10 17:20:43 +0200

    ajout des fichiers de genomestudio

---
+++ R/controlData.R
@@ -0,0 +1,6 @@
+path <- getwd()
+controlProb  <- read.table(paste(path,"/../Data/ControlProbeprof.txt",sep = ""), sep = "\t",dec = ",", header = TRUE ,as.is = TRUE)
+controlGene  <- read.table(paste(path,"/../Data/ControlGeneprof.txt",sep = ""), sep = "\t",dec = ",", header = TRUE ,as.is = TRUE)
+controlSample  <- read.table(paste(path,"/../Data/TableSampleControl.txt",sep = ""), sep = "\t",dec = ",", header = TRUE ,as.is = TRUE)
+hyb <- controlProb[controlProb$TargetID=="CY3_HYB",]
+biot <- controlProb[controlProb$TargetID=="BIOTIN",]
