13e9006162494bf9ddef90836c1e9c4028af7c5b
IntsiraL <martin.rasoloniaina@yahoo.fr>
2017-05-18 16:30:50 +0200

    mise à jour du rapport

--- R/qa_metrics.R
+++ R/qa_metrics.R
@@ -10,7 +10,7 @@ ht12metrics  <- read.table(paste(path,"/../Data/200729890044/Metrics.txt",sep = 
 ht12snr  <- ht12metrics$P95Grn/ht12metrics$P05Grn
 labs  <- paste(ht12metrics[, 2],  ht12metrics[, 3], sep = "_")
 par(mai = c(1.5, 0.8, 0.3, 0.1))
-plot (1:12 ,  ht12snr , pch = 19, ylab = "P95 / P05", xlab = "",main = "Signal -to-noise  ratio  for  200729890044  data", axes = FALSE ,
+plot (1:12 , type = "b",  ht12snr , pch = 19, ylab = "P95 / P05", xlab = "",main = "Signal -to-noise  ratio  for  200729890044  data", axes = FALSE ,
       frame.plot = TRUE)
 axis (2)
 axis(1, 1:12, nameCol , las = 2)
@@ -30,7 +30,7 @@ for (i in c("Co","AL","LPS","AL_LPS")){
 ht12metrics  <- read.table(paste(path,"/../Data/200796240064/Metrics.txt",sep = ""), sep = "\t", header = TRUE ,as.is = TRUE)
 ht12snr  <- ht12metrics$P95Grn/ht12metrics$P05Grn
 par(mai = c(1.5, 0.8, 0.3, 0.1))
-plot (1:12 ,  ht12snr , pch = 19, ylab = "P95 / P05", xlab = "",main = "Signal -to-noise  ratio  for  200796240064  data", axes = FALSE ,
+plot (1:12 ,  ht12snr ,type = "b", pch = 19, ylab = "P95 / P05", xlab = "",main = "Signal -to-noise  ratio  for  200796240064  data", axes = FALSE ,
       frame.plot = TRUE)
 axis (2)
 axis(1, 1:12, nameCol , las = 2)
